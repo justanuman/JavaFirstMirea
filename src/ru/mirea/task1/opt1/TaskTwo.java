fde0a9bcc0177c836bc5897d3ad1782563479ffb
justanuman <45141610+justanuman@users.noreply.github.com>
2021-09-15 11:39:06 +0300

    я получил лабы только недавно так что пока делал практики начал лабы

---
+++ src/ru/mirea/task1/opt1/TaskTwo.java
@@ -0,0 +1,11 @@
+package ru.mirea.task1.opt1;
+
+public class TaskTwo {
+
+    public static void main(String[] args) {
+       int i=0;
+        for (String str : args) {
+            i++;
+        } System.out.println(i);
+    }
+}
