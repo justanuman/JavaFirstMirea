4e15aed55ca2a2f1ccf0582a7ce177b8d2484b4a
justanuman <45141610+justanuman@users.noreply.github.com>
2021-09-15 10:44:00 +0300

    I hope nothing I wont break everything

---
+++ src/ru/mirea/task4/task2/Ball.java
@@ -0,0 +1,36 @@
+package ru.mirea.task4.task2;
+
+public class Ball {
+    private double x =  0.0;
+    private double y =  0.0;
+    public Ball(double x,double y)
+    {
+        this.x=x;
+        this.y=y;
+    }
+    public Ball(){}
+    public double getX()
+    {
+        return x;
+    }
+    public void setX(double x)
+    {
+      this.x=x;
+    }
+    public double getY()
+    {
+        return y;
+    }
+    public void setY(double x)
+    {
+        this.y=x;
+    }
+    public void move( double xDisp, double yDisp){
+        x+=xDisp;
+        y+=yDisp;
+    }
+    @Override
+    public String toString() {
+        return "Ball @ ("+this.x+", "+this.y+").";
+    }
+}
---
+++ src/ru/mirea/task7/task2/Movable.java
@@ -0,0 +1,8 @@
+package ru.mirea.task7.task2;
+
+interface Movable {
+    public  void moveUp();
+    public  void moveDown();
+    public  void moveLeft();
+    public  void moveRight();
+}
