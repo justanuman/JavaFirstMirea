94cb7b09ccf9bfcd0ddb47e8adb8914739bf4a8a
justanuman <45141610+justanuman@users.noreply.github.com>
2021-09-22 13:57:06 +0300

    почти сделал всю 5

--- src/ru/mirea/task5/furniture/Furniture.java
+++ src/ru/mirea/task5/furniture/Furniture.java
@@ -1,41 +1,4 @@
 package ru.mirea.task5.furniture;
 
-public abstract class Furniture {
-    protected String Size;
-    protected String Tip;
-    protected String Mat;
-    protected int Price;
-    public Furniture(String Tip,  String Size, String Mat){
-        setSize(Size);
-        setTip(Tip);
-        setMat(Mat);
-        setPrice(0);
-    }
-
-    protected Furniture() {
-    }
-
-    public void setMat(String mat) {
-        Mat = mat;
-    }
-
-    public void setPrice(int price) {
-        Price = price;
-    }
-
-    public abstract String getMat() ;
-
-    public abstract int getPrice();
-
-    public abstract String getSize();
-
-    public abstract String getTip() ;
-
-    public void setTip(String tip) {
-        Tip = tip;
-    }
-    public void setSize(String size) {
-        Size = size;
-    }
-    public abstract String toString();
+public class Furniture {
 }
