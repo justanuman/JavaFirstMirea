209ebad2ba1c6226ef778ebe406a01db4f2deb60
justanuman <45141610+justanuman@users.noreply.github.com>
2021-09-22 09:49:42 +0300

    я получил лабы только недавно так что пока делал практики начал лабы

---
+++ src/ru/mirea/task8/Shapes.java
@@ -0,0 +1,9 @@
+package ru.mirea.task8;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+public class Shapes extends JFrame {
+    JFrame frame = new JFrame ("Authority");
+
+}
