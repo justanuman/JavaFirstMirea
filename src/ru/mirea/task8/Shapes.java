3261e9329379bde1f3f194f77f74fd84e90fc6fb
justanuman <45141610+justanuman@users.noreply.github.com>
2021-09-29 11:52:37 +0300

    новые файлы, доделаю 8 и рекурсию позже

--- src/ru/mirea/task8/Shapes.java
+++
@@ -1,9 +0,0 @@
-package ru.mirea.task8;
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-public class Shapes extends JFrame {
-    JFrame frame = new JFrame ("Authority");
-
-}
